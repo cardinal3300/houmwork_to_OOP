637bf72cc2f0db28732ceca0f5cc13d510148c32
Zherdev Igor <cardinal3300@.com>
2025-08-03 13:28:32 +0300

    fix houmwork_14_2

--- src/category.py
+++ src/category.py
@@ -1,3 +1,5 @@
+from typing import Optional
+
 from src.product import Product
 
 
@@ -15,25 +17,25 @@ class Category:
     category_count = 0  # количество созданных категорий
     product_count = 0  # Общее количество товаров во всех категориях
 
-    def __init__(self, name: str, description: str, products: list[Product]):
+    def __init__(self, name: str, description: str, products: Optional[list[Product]]) -> None:
         """Инициализация категории.
         Args:
             name (str): Название категории.
             description (str): Описание категории.
             products (List[Product], optional): Список товаров в категории.
         """
-
         self.name = name
         self.description = description
         self.__products: list[Product] = []
-        for product in products:
-            self.add_product(product)
-
+        if products:
+            for product in products:
+                self.add_product(product)
         Category.category_count += 1
 
     def add_product(self, product: Product) -> None:
         """Добавляет продукт в категорию, если он является экземпляром Product."""
-
+        if not isinstance(product, Product):
+            raise TypeError("Можно добавлять только объекты класса Product или его подклассов.")
         self.__products.append(product)
         Category.product_count += 1
 
