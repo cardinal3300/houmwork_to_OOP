f9cf983380e2aa3f15462907202c78bf9e1e9f21
Zherdev Igor <cardinal3300@.com>
2025-08-16 14:16:47 +0300

    add LoggerMixin

--- src/product.py
+++ src/product.py
@@ -1,11 +1,17 @@
 from src.base_product import BaseProduct
-
+from src.mixins import LoggerMixin
 
 class Product(BaseProduct):
     """Класс, представляющий товар."""
 
     def __init__(self, name: str, description: str, price: float, quantity: int) -> None:
         super().__init__(name, description, price, quantity)
+
+
+    def __repr__(self) -> str:
+        return (f"{self.__class__.__name__}("
+                f"name={self.name!r}, description={self.description!r}, "
+                f"price={self.price!r}, quantity={self.quantity!r})")
 
 
 class Smartphone(Product):
@@ -29,6 +35,14 @@ class Smartphone(Product):
         self.color = color
 
 
+    def __repr__(self) -> str:
+        return (f"{self.__class__.__name__}("
+                f"name={self.name!r}, description={self.description!r}, "
+                f"price={self.price!r}, quantity={self.quantity!r}, "
+                f"efficiency={self.efficiency!r}, model={self.model!r}, "
+                f"memory={self.memory!r}, color={self.color!r})")
+
+
 class LawnGrass(Product):
     """Класс для хранения данных о газонной траве."""
 
@@ -46,3 +60,11 @@ class LawnGrass(Product):
         self.country = country
         self.germination_period = germination_period
         self.color = color
+
+
+    def __repr__(self) -> str:
+        return (f"{self.__class__.__name__}("
+                f"name={self.name!r}, description={self.description!r}, "
+                f"price={self.price!r}, quantity={self.quantity!r}, "
+                f"country={self.country!r}, germination_period={self.germination_period!r}, "
+                f"color={self.color!r})")
